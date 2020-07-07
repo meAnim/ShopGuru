d73910b58c7281aafca4cb7a253fb320e5f27246
FalconZ <animj.emma@gmail.com>
2020-07-07 14:23:39 +0100

    spring boot app set up

---
+++ src/main/java/com/eshlon/shopGuru/ShopGuruApplication.java
@@ -0,0 +1,13 @@
+package com.eshlon.shopGuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopGuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshlon/shopGuru/ShopGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshlon.shopGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
