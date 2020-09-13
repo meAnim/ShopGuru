8f7c8e3b9492d71c2f96182b6bb376ff5701b22e
FalconZ <animj.emma@gmail.com>
2020-09-13 13:54:35 +0100

    user class id added

--- src/main/java/com/eshlon/shopGuru/model/User.java
+++ src/main/java/com/eshlon/shopGuru/model/User.java
@@ -13,7 +13,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-import javax.validation.constraints.NotEmpty;
+//import javax.validation.constraints.NotEmpty;
 
 import com.sun.istack.NotNull;
 
@@ -26,117 +26,6 @@ public class User {
 	@Column(name = "auth_user_id")
 	private int id;
 
-//	@NotEmpty(message = "First name is required")
-	@Column(name = "first_name", nullable = false)
-	private String firstname;
 	
-//	@NotEmpty(message = "Last name is required")
-	@Column(name = "last_name", nullable = false)
-	private String lastname;
-	
-//	@NotEmpty(message = "email is required")
-	@Column(name = "email", nullable = false)
-	private String email;
-	
-//	@NotEmpty(message = "password is required")
-	@Column(name = "password", nullable = false)
-	private String password;
-	
-	@Transient
-	private String vPassword;
-
-	@Column(name = "status", nullable = false)
-	private String status;
-
-	@ManyToMany(cascade = CascadeType.ALL)
-	@JoinTable(name = "auth_user_role", joinColumns = @JoinColumn(name = "auth_user_id"), inverseJoinColumns = @JoinColumn(name = "auth_role_id"))
-	
-	private Set<Role> roles;
-	
-
-	public User(String firstname, String lastname, String email, String password, String status, Set<Role> roles) {
-		super();
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.email = email;
-		this.password = password;
-		this.status = status;
-		this.roles = roles;
-	}
-	
-	public User() {
-		
-	}
-	
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getvPassword() {
-		return vPassword;
-	}
-
-	public void setvPassword(String vPassword) {
-		this.vPassword = vPassword;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public Set<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
-
-	@Override
-	public String toString() {
-		return "User [firstname=" + firstname + ", lastname=" + lastname + ", email=" + email + ", password=" + password
-				+ ", vPassword=" + vPassword + ", status=" + status + ", roles=" + roles + "]";
-	}
 
 }
