0c5b028b3cf918569160e78aeff0e9b6f9816d7c
FalconZ <animj.emma@gmail.com>
2020-09-13 14:32:09 +0100

    role attribute added

--- src/main/java/com/eshlon/shopGuru/model/User.java
+++ src/main/java/com/eshlon/shopGuru/model/User.java
@@ -44,6 +44,9 @@ public class User {
 	@Column(name = "status", nullable = false)
 	private String status;
 
-
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "auth_user_role", joinColumns = @JoinColumn(name = "auth_user_id"), inverseJoinColumns = @JoinColumn(name = "auth_role_id"))
+	
+	private Set<Role> roles;
 
 }
