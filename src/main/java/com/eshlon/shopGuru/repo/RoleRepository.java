e4ebd4d879221a9ec1b1c38eaabee1218365a4a9
FalconZ <animj.emma@gmail.com>
2020-09-14 20:34:01 +0100

    findByrole method created

--- src/main/java/com/eshlon/shopGuru/repo/RoleRepository.java
+++ src/main/java/com/eshlon/shopGuru/repo/RoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.eshlon.shopGuru.model.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer>{
-
+	Role findbyRole ();
 }
