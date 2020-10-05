0b0b14fb542bc4ef2fe3c668b505dee4b1d2bc4f
FalconZ <animj.emma@gmail.com>
2020-10-05 18:18:09 +0100

    index.jsp page added

--- src/main/java/com/eshlon/shopGuru/controller/HomeController.java
+++ src/main/java/com/eshlon/shopGuru/controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.eshlon.shopGuru.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
-	public String homeController() {
-		return "Hello world";
+public class HomeController {  
+	@RequestMapping("/")
+	public String home() {
+		return "views/index.jsp";
 	}
 }
