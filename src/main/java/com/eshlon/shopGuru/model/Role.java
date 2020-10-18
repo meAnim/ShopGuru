10a1b9719e44799e9852866cea3cc08db15c62ef
FalconZ <animj.emma@gmail.com>
2020-10-18 16:10:27 +0100

    setters and getters added for role model attributes

--- src/main/java/com/eshlon/shopGuru/model/Role.java
+++ src/main/java/com/eshlon/shopGuru/model/Role.java
@@ -10,4 +10,25 @@ public class Role {
 	private int id;
 	private String role;
 	private String desc;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
 }
