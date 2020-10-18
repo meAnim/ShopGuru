0a452cfb59159abdf2f0df67f465ad46a4a99db1
FalconZ <animj.emma@gmail.com>
2020-10-18 16:08:43 +0100

    role attribute and ID added to role model

--- src/main/java/com/eshlon/shopGuru/model/Role.java
+++ src/main/java/com/eshlon/shopGuru/model/Role.java
@@ -1,5 +1,12 @@
 package com.eshlon.shopGuru.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "auth_role")
 public class Role {
 
+	private int id;
+	private String role;
 }
