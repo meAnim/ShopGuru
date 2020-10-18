1ce222438506a008028e4f50b8500efb92f9fc8f
FalconZ <animj.emma@gmail.com>
2020-10-18 16:57:11 +0100

    rolerepository created

---
+++ src/main/java/com/eshlon/shopGuru/repo/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.eshlon.shopGuru.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eshlon.shopGuru.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
