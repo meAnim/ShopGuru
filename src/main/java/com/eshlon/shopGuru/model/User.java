0036f4aaaba56a747ef8f6bb90f8a34ae8759c8d
FalconZ <animj.emma@gmail.com>
2020-10-18 13:23:00 +0100

    getters and setters generated for user model

--- src/main/java/com/eshlon/shopGuru/model/User.java
+++ src/main/java/com/eshlon/shopGuru/model/User.java
@@ -18,4 +18,56 @@ public class User {
 	private String vPassword;
 	private String status;
 	private List<Role> roles;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getvPassword() {
+		return vPassword;
+	}
+	public void setvPassword(String vPassword) {
+		this.vPassword = vPassword;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
 }
