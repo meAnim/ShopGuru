ed908cea5758ab548d02c8b63dcc888de7ddf576
FalconZ <animj.emma@gmail.com>
2020-10-19 15:05:52 +0100

    httpsecurity method added to securityConfig class

--- src/main/java/com/eshlon/shopGuru/config/AppSecurityConfig.java
+++ src/main/java/com/eshlon/shopGuru/config/AppSecurityConfig.java
@@ -3,6 +3,7 @@ package com.eshlon.shopGuru.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,5 +23,11 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService)			
 			.passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+			
 	}
 }
