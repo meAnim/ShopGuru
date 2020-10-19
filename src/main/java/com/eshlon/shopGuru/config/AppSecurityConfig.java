1ffc2b235f43021d66459468bf132f4a9cea86d0
FalconZ <animj.emma@gmail.com>
2020-10-19 14:57:50 +0100

    userDetailsService autowired into securityConfig class

--- src/main/java/com/eshlon/shopGuru/config/AppSecurityConfig.java
+++ src/main/java/com/eshlon/shopGuru/config/AppSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -12,4 +13,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	UserDetailsService userDetailsService;
 }
