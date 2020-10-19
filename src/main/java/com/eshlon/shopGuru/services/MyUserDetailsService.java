d92ee420f0d047a335190069176c3c552785d4de
FalconZ <animj.emma@gmail.com>
2020-10-19 19:33:29 +0100

    loadUserByUsername method in MyUserDetailsService is completed

--- src/main/java/com/eshlon/shopGuru/services/MyUserDetailsService.java
+++ src/main/java/com/eshlon/shopGuru/services/MyUserDetailsService.java
@@ -1,12 +1,24 @@
 package com.eshlon.shopGuru.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.eshlon.shopGuru.model.User;
+import com.eshlon.shopGuru.services.MyUserDetails;
 import com.eshlon.shopGuru.repo.UserRepository;
 
 public class MyUserDetailsService implements UserDetailsService{
 	
 	@Autowired
 	UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(userName);
+		MyUserDetails myUserDetails = new MyUserDetails(user);
+		System.out.println(myUserDetails.getAuthorities().toString());
+		return myUserDetails; 
+	}
 }
