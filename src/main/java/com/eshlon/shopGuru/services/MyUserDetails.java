c48bbd8e58afdb066f886831199908cdd982bb53
FalconZ <animj.emma@gmail.com>
2020-10-19 18:28:01 +0100

    granted authorities method completed

--- src/main/java/com/eshlon/shopGuru/services/MyUserDetails.java
+++ src/main/java/com/eshlon/shopGuru/services/MyUserDetails.java
@@ -1,5 +1,11 @@
 package com.eshlon.shopGuru.services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.eshlon.shopGuru.model.User;
@@ -14,5 +20,16 @@ public class MyUserDetails implements UserDetails{
 	
 	public MyUserDetails() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		this.user.getRoles().forEach(p->{
+			GrantedAuthority authority = new SimpleGrantedAuthority(p.getRole().toString());
+			authorities.add(authority);
+		});
+		return authorities;
 	}
 }
