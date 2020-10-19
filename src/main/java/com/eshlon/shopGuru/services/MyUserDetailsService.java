643b56f51e6dd07a34aae70192450a83c1a5a6d1
FalconZ <animj.emma@gmail.com>
2020-10-19 19:15:47 +0100

    UserRepository authowired in MyUserDetailsService

--- src/main/java/com/eshlon/shopGuru/services/MyUserDetailsService.java
+++ src/main/java/com/eshlon/shopGuru/services/MyUserDetailsService.java
@@ -1,7 +1,12 @@
 package com.eshlon.shopGuru.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-public class MyUserDetailsService implements UserDetailsService{
+import com.eshlon.shopGuru.repo.UserRepository;
 
+public class MyUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	UserRepository userRepo;
 }
