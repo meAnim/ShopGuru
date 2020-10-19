409787f6354eadb13e6d75919b9f2752d1677159
FalconZ <animj.emma@gmail.com>
2020-10-19 15:00:18 +0100

    AuthenticationManagerBuilder method added to securityConfig class

--- src/main/java/com/eshlon/shopGuru/config/AppSecurityConfig.java
+++ src/main/java/com/eshlon/shopGuru/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.eshlon.shopGuru.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,4 +17,10 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	UserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService)			
+			.passwordEncoder(bCryptPasswordEncoder);
+	}
 }
